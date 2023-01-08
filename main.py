413422c9282862a822bf10d8f76f51d18f112ff3
BiggieFudge <73502334+BiggieFudge@users.noreply.github.com>
2023-01-08 13:54:02 +0200

    Update

--- main.py
+++ main.py
@@ -33,7 +33,8 @@ mainDict = {}
 roadsWithNoData = []
 
 # Choose year (2021)
-driver.find_element(By.XPATH,
+for i in range(2):
+    driver.find_element(By.XPATH,
                     value='//*[@id="ctl00_ctl00_ContentPlaceMain_contentPageMain_YearComboBox_Input"]').send_keys(2)
 driver.find_element(By.XPATH,
                     value='//*[@id="ctl00_ctl00_ContentPlaceMain_contentPageMain_YearComboBox_Input"]').send_keys(
@@ -41,28 +42,48 @@ driver.find_element(By.XPATH,
 
 for roadNumber in driver.find_element(By.CLASS_NAME, value='rcbList').get_attribute('innerHTML').split('</li>'):
     roadNumber = cleanhtml(roadNumber)
-    print(roadNumber)
+
     driver.find_element(By.XPATH, value='//*[@id="ctl00_ctl00_ContentPlaceMain_contentPageMain_Road_Input"]').clear()
     driver.find_element(By.XPATH, value='//*[@id="ctl00_ctl00_ContentPlaceMain_contentPageMain_Road_Input"]').send_keys(roadNumber)
     time.sleep(1)
     driver.find_element(By.XPATH, value='//*[@id="ctl00_ctl00_ContentPlaceMain_contentPageMain_Road_Input"]').send_keys(
         Keys.ENTER)
-    time.sleep(1)
-    # junctionsList = list(driver.find_element(By.XPATH, value='//*[@id="ctl00_ctl00_ContentPlaceMain_contentPageMain_Junction1_DropDown"]').get_attribute('innerHTML').split('</li>'))
-    driver.find_element(By.XPATH, value='//*[@id="SearchButton"]').click()
-    time.sleep(1)
+    time.sleep(2)
     try:
-        collisionData = driver.find_element(By.CLASS_NAME, value='resultset').get_attribute('innerHTML')
+        junctionsList = list(driver.find_element(By.XPATH, value='//*[@id="ctl00_ctl00_ContentPlaceMain_contentPageMain_Junction1_DropDown"]').get_attribute('innerHTML').split('</li>'))
     except Exception as e:
-        print(e)
-        print('No data for road number: ' + roadNumber)
-        roadsWithNoData.append(roadNumber)
+        print('No junctions for road number: ' + roadNumber)
         continue
-    collisionData = collisionData.split('</div>')
-    secondaryDict = {}
-    for index in range(2, len(collisionData[2:]) - 2, 2):
-        secondaryDict.update({cleanhtml(collisionData[index]): cleanhtml(collisionData[index + 1])})
-    mainDict.update({roadNumber: secondaryDict})
+    for idx, junction in enumerate(junctionsList[0:len(junctionsList)-2]):
+        junctionFrom = cleanhtml(junction)
+        junctionTo = cleanhtml(junctionsList[idx+1])
+        print(roadNumber + ' ' + junctionFrom + ' ' + junctionTo)
+        try:
+            driver.find_element(By.XPATH,value='//*[@id="ctl00_ctl00_ContentPlaceMain_contentPageMain_Junction1_Input"]').clear()
+            driver.find_element(By.XPATH, value='//*[@id="ctl00_ctl00_ContentPlaceMain_contentPageMain_Junction1_Input"]').send_keys(junctionFrom)
+            time.sleep(1)
+            driver.find_element(By.XPATH, value='//*[@id="ctl00_ctl00_ContentPlaceMain_contentPageMain_Junction1_Input"]').send_keys(Keys.ENTER)
+            driver.find_element(By.XPATH, value='//*[@id="ctl00_ctl00_ContentPlaceMain_contentPageMain_Junction2_Input"]').clear()
+            driver.find_element(By.XPATH, value='//*[@id="ctl00_ctl00_ContentPlaceMain_contentPageMain_Junction2_Input"]').send_keys(junctionTo)
+            time.sleep(1)
+            driver.find_element(By.XPATH, value='//*[@id="ctl00_ctl00_ContentPlaceMain_contentPageMain_Junction2_Input"]').send_keys(Keys.ENTER)
+        except:
+            print('unknown error occured')
+            continue
+        driver.find_element(By.XPATH, value='//*[@id="SearchButton"]').click()
+        time.sleep(1)
+        try:
+            collisionData = driver.find_element(By.CLASS_NAME, value='resultset').get_attribute('innerHTML')
+        except Exception as e:
+            print(e)
+            print('No data for road number: ' + roadNumber + ' junctions: ' + junctionFrom + ' - ' + junctionTo)
+            roadsWithNoData.append(roadNumber)
+            continue
+        collisionData = collisionData.split('</div>')
+        secondaryDict = {}
+        for index in range(2, len(collisionData[2:]) - 2, 2):
+            secondaryDict.update({cleanhtml(collisionData[index]): cleanhtml(collisionData[index + 1])})
+        mainDict.update({roadNumber +'::' + junctionFrom + ':'+ junctionTo: secondaryDict})
 
 dataBase = pd.DataFrame.from_dict(mainDict, orient='index')
 dataBase.fillna(0, inplace=True)
